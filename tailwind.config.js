05f841eea8bf32c3fd29f68418910705e1aca5cb
MASSON Charlie <charlie.mass.cm@gmail.com>
2023-03-06 13:31:21 +0100

    fix pour Louka

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
       "./templates/**/*.html.twig",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'newBlue': '#000000',
+        },
+      }
     },
     plugins: [],
 }
