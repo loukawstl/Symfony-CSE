42d81adcf919311ea2399018b658c9022df969d7
loukawstl <louka.wasteels60@gmail.com>
2023-03-13 10:06:32 +0100

    test

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
