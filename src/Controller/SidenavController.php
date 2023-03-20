f95f032d2582795c5e1541961897c83264b8ffa2
MASSON Charlie <charlie.mass.cm@gmail.com>
2023-03-20 15:17:58 +0100

    sidenav progress

--- src/Controller/SidenavController.php
+++ src/Controller/SidenavController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SidenavPartnershipController extends AbstractController
+class SidenavController extends AbstractController
 {
 
     private $partnershipRepository;
