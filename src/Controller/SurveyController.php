1e9a3c2882d8d507282886553a5a3b0a446ad8d4
loukawstl <louka.wasteels60@gmail.com>
2023-03-27 15:18:23 +0200

    back

---
+++ src/Controller/SurveyController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\SurveyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SurveyController extends AbstractController
+{
+    /**
+     * @Route("/surveys", name="survey_index")
+     */
+}
--- src/Form/PartnershipType.php
+++ src/Form/PartnershipType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\Partnership;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,7 +26,29 @@ class PartnershipType extends AbstractType
                 'label' => 'Lien :'
             ])
             ->add('file', FileType::class, [
-                'label' => 'Images :'
+                'label' => 'Image :',
+                'required' => false,
+                'mapped' => false,
+                'multiple' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/svg+xml',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Téléchargez une image JPG, PNG, SVG ou WebP',
+                        'maxSize' => '5M',
+                        'maxSizeMessage' => 'L\image est trop large. La taille maximum permise est {{ limit }} {{ suffix }}',
+                    ]) 
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => $builder->getOption('on_edit') ? 'Modifier' : 'Créer',
+            'attr' => [
+                'class' => $builder->getOption('on_edit') ? 'bg-blue-900 text-white p-2 rounded-lg hover:bg-blue-600' : 'bg-green-800 text-white p-3 rounded-lg hover:bg-green-600',
+            ],
             ])
         ;
     }
@@ -33,6 +57,7 @@ class PartnershipType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Partnership::class,
+            'on_edit' => false,
         ]);
     }
 }
