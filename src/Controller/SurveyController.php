552b711fb052b7e14b88cfd41cd99711068b04a7
MASSON Charlie <charlie.mass.cm@gmail.com>
2023-05-05 09:55:14 +0200

    survey can now be replied to

--- src/Controller/SurveyController.php
+++ src/Controller/SurveyController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Survey;
 use App\Entity\SurveyAnswer;
 use App\Repository\SurveyRepository;
@@ -9,6 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+
 
 
 class SurveyController extends AbstractController
@@ -19,18 +23,32 @@ class SurveyController extends AbstractController
         $survey = $surveyRepository->find($request->request->get('surveyId'));
         $optionId = $request->request->get('surveyOption');
         $check = true;
+        $surveyName = 'survey'.$survey->getId();
 
-        if ($optionId = null){
-            $this->addFlash('surveyError', 'Erreur: Vous ne pouvez pas envoyer un formulaire vide.');
+
+        if ($request->cookies->get($surveyName)){
+            $this->addFlash('surveyError', 'Erreur: Vous avez déjà voté à ce sondage.');
             $check = false;
         }
 
-        dd($optionId);
+        if ($optionId === null){
+            $this->addFlash('surveyError', 'Erreur: Vous ne pouvez pas envoyer un formulaire vide.');
+            $check = false;
+        }
 
         if ($check){
             $surveyAnswer = new SurveyAnswer();
             $surveyAnswer->setAnswer($optionId);
             $surveyAnswer->setSurvey($survey);
+            $cookie = new Cookie( 
+                $surveyName,                             // name
+                true,                                    // content
+                time() + 36000,                          // expiration date
+            );
+
+            $response = new Response();
+            $response->headers->setCookie($cookie);
+            $response->send();
 
             $manager->persist($surveyAnswer);
             $manager->flush();
@@ -38,12 +56,6 @@ class SurveyController extends AbstractController
 
         $referer = $request->headers->get('referer');
         return new RedirectResponse($referer);
-    }
-
-    #[Route('/confirmation', name: 'app_confirmation')]
-    public function confirmation(): Response
-    {
-        return $this->render('sidebar_left/confirmation.html.twig');
     }
     
 }
